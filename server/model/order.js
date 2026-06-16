9dd7c9da7018a44a48179ad99d5c16b294b4ed52
singhgursharanpreet <gursharan101000@gmail.com>
2026-06-16 17:03:01 +1200

    Fix routes and render port

--- server/model/order.js
+++ server/model/order.js
@@ -25,4 +25,4 @@ const orderSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Order", orderSchema);
+module.exports = mongoose.model("order", orderSchema);
